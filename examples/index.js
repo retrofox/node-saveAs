b28020784142c86cfe5e050a29853345bd3f4ac1
Damian Suarez <rdsuarez@gmail.com>
2011-05-19 01:20:50 -0300

    download images

--- examples/index.js
+++ examples/index.js
@@ -2,20 +2,29 @@
 var saveAs = require('../lib/saveas');
 
 // request options
-var reqOpts = {
-      host: 'about.me'
-    , port: 80
-    , method: 'GET'
-//    , headers: req.headers
-    , path: '/retrofox'
-  }
+var options = {
+      request: {
+          host: 'about.me'
+        , port: 80
+        , method: 'GET'
+        , path: '/retrofox'
+      }
+      , down: {
+          images: true
+        , css: true
+        , js: true
+      }
+      , AllowFromExternalHost: true
+      , folders: {
+          images: './images'
+        , js: '/javascript'
+        , css: '/styles'
+      }
+    }
 
 
 // init request to google
-saveAs.save(reqOpts, 'about.me.retrofox', function(err, stats) {
-  console.log("-- data ----------------------------------------------------");
-  console.log(err);
-  console.log(stats);
-  console.log("---------------------------------------------------- data --");
+saveAs.save(options, 'about.me.retrofox', function(err, stats) {
+  //console.log(stats);
 })
 
